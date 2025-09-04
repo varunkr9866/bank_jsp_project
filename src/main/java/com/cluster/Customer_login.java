b36d3f181c1bb1234be18cb5f1b70713a1a4faff
varun <varunkr9866@gmail.com>
2025-09-04 11:57:51 +0530

    STARTED BANK WEB APPLICATION PROJECT

---
+++ src/main/java/com/cluster/Customer_login.java
@@ -0,0 +1,12 @@
+package com.cluster;
+
+
+import jakarta.servlet.ServletConfig;
+import jakarta.servlet.http.HttpServlet;
+
+public class Customer_login extends HttpServlet{
+	public void init(ServletConfig config) {
+			
+		
+	}
+}

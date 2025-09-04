bd9f6bc9e58c200a494841a7a99308213fb84811
varun <varunkr9866@gmail.com>
2025-09-04 13:13:14 +0530

    GETTING CONNECTION

--- src/main/java/com/cluster/Customer_login.java
+++ src/main/java/com/cluster/Customer_login.java
@@ -1,12 +1,24 @@
 package com.cluster;
 
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.http.HttpServlet;
 
 public class Customer_login extends HttpServlet{
+	Connection con= null;
 	public void init(ServletConfig config) {
-			
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql:localhost:3306varunmysql", "root", "Cluster");
+			System.out.println("Connection Established");
+		} catch (ClassNotFoundException | SQLException e) {
+			System.out.println("Caught Exception:" +e);
+			e.printStackTrace();
+		}
 		
 	}
 }

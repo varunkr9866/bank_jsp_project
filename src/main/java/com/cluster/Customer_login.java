af0478e4af48f5e5f6967c17b451dc2943171255
varun <varunkr9866@gmail.com>
2025-09-04 14:04:36 +0530

    GETTING CONNECTION FOT REGISTER SERVLET

--- src/main/java/com/cluster/Customer_login.java
+++ src/main/java/com/cluster/Customer_login.java
@@ -1,24 +1,22 @@
 package com.cluster;
 
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletConfig;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 public class Customer_login extends HttpServlet{
-	Connection con= null;
-	public void init(ServletConfig config) {
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con = DriverManager.getConnection("jdbc:mysql:localhost:3306varunmysql", "root", "Cluster");
-			System.out.println("Connection Established");
-		} catch (ClassNotFoundException | SQLException e) {
-			System.out.println("Caught Exception:" +e);
-			e.printStackTrace();
-		}
+public void service(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		
+		RequestDispatcher reqdis = req.getRequestDispatcher("./jsp/login.jsp");
+		reqdis.forward(req, res);
 	}
 }
